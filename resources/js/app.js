86c0d86214c5f43311d21ad6f40f3bde87c111b7
Darren Henderson <darrenhusa@yahoo.com>
2021-11-02 13:22:03 -0500

    Got minimal Vue2 highchart component syntaz to work!!!!

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 
 // window.Vue = require('vue');
  Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+ Vue.component('line-chart', require('./components/LineChart.vue').default);
  const app = new Vue({
    el: '#app',
  });
