1ff7869c48c3d9a63317170694fe4c7b5aa3a2c4
Darren Henderson <darrenh@ccsj.edu>
2021-11-06 20:31:34 -0500

    Corrected code to display the highcharts chart properly!!!

--- public/js/app.js
+++ public/js/app.js
@@ -2325,186 +2325,20 @@ highcharts_modules_exporting__WEBPACK_IMPORTED_MODULE_2___default()((highcharts_
   data: function data() {
     return {
       results: [],
-      series: []
+      series: [],
+      chartOptions: {}
     };
   },
-  computed: {
-    mapDataForChart: function mapDataForChart() {
-      var number = this.results.data.length; // const number = response.data.data.length;
-      // const number = 3;
-
-      console.log(number); // let seriesTemp = [];
-
-      for (var i = 0; i < number; i++) {
-        this.series.push({
-          name: this.results.data.data[i].name,
-          data: this.results.data.data[i].data
-        }); // end push
-      } // end for
-
-    } // end mapDataForChart
-
-  },
-  // end computed
   created: function created() {
-    console.log("stacked column chart 2 created"); //   axios.get(this.url)
-    //         .then(response => {
-    //             this.results = response.data;
-    //             const number = this.results.data.length;
-    //             console.log(number);
-    //             // let seriesTemp = [];
-    //             for(let i=0; i<number; i++)
-    //             {
-    //                 // console.log(this.results.data.data[i].name);
-    //                 // console.log(this.results.data.data[i].data);
-    //                 this.series.push({
-    //                     name: response.data.data[i].name,
-    //                     data: response.data.data[i].data,
-    //                     // name: this.results.data.data[i].name,
-    //                     // data: this.results.data.data[i].data,
-    //                 }); // end push
-    //             } // end for
-    //         }); //end axios get then
-    // const number = response.data.data.length;
-    // const number = 3;
-    //   this.mapDataForChart();
-
-    var self = this; //   self.fetchData();
-
-    this.fetchData(); //   self.mapDataForChart(self);    
-    //   this.mapDataForChart(this);    
-
-    this.mapDataForChart();
+    //   console.log("stacked column chart 2 created");
+    this.fetchData();
   },
-  mounted: function mounted() {
-    console.log("inside mounted"); //   this.mapDataForChart(this);
-    //   console.log(this.url);
-    //   let self = this;
-    //   let self = this;
-
-    console.log('why is this undefined???');
-    console.log(this.results); // console.log(self.results);
-    // console.log(self.results.title);
-
-    var chartOptions = {
-      chart: {
-        type: 'column'
-      },
-      title: {
-        text: this.results.title
-      },
-      xAxis: {
-        categories: this.results.categories
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: this.results.y_axis
-        },
-        stackLabels: {
-          enabled: true,
-          style: {
-            fontWeight: 'bold',
-            color: // theme
-            (highcharts__WEBPACK_IMPORTED_MODULE_1___default().defaultOptions.title.style) && (highcharts__WEBPACK_IMPORTED_MODULE_1___default().defaultOptions.title.style.color) || 'gray'
-          }
-        }
-      },
-      legend: {
-        align: 'right',
-        x: -30,
-        verticalAlign: 'top',
-        y: 25,
-        floating: true,
-        backgroundColor: (highcharts__WEBPACK_IMPORTED_MODULE_1___default().defaultOptions.legend.backgroundColor) || 'white',
-        borderColor: '#CCC',
-        borderWidth: 1,
-        shadow: false
-      },
-      tooltip: {
-        headerFormat: '<b>{point.x}</b><br/>',
-        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-      },
-      plotOptions: {
-        column: {
-          stacking: 'normal',
-          dataLabels: {
-            enabled: true
-          }
-        }
-      },
-      series: this.series
-    }; // test chart
-
-    highcharts__WEBPACK_IMPORTED_MODULE_1___default().chart(this.$el, {
-      title: {
-        text: this.results.title
-      },
-      subtitle: {
-        text: 'Source: thesolarfoundation.com'
-      },
-      yAxis: {
-        title: {
-          text: 'Number of Employees'
-        }
-      },
-      xAxis: {
-        accessibility: {
-          rangeDescription: 'Range: 2010 to 2017'
-        }
-      },
-      legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
-      },
-      plotOptions: {
-        series: {
-          label: {
-            connectorAllowed: false
-          },
-          pointStart: 2010
-        }
-      },
-      series: [{
-        name: 'Installation',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-      }, {
-        name: 'Manufacturing',
-        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-      }, {
-        name: 'Sales & Distribution',
-        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-      }, {
-        name: 'Project Development',
-        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-      }, {
-        name: 'Other',
-        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-      }],
-      responsive: {
-        rules: [{
-          condition: {
-            maxWidth: 500
-          },
-          chartOptions: {
-            legend: {
-              layout: 'horizontal',
-              align: 'center',
-              verticalAlign: 'bottom'
-            }
-          }
-        }]
-      }
-    }); // Highcharts.chart(this.$el, chartOptions); // end chart
-  },
-  // end mounted
   methods: {
     fetchData: function fetchData() {
       var _this = this;
 
       return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee() {
-        var response;
+        var response, number, i;
         return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee$(_context) {
           while (1) {
             switch (_context.prev = _context.next) {
@@ -2515,51 +2349,87 @@ highcharts_modules_exporting__WEBPACK_IMPORTED_MODULE_2___default()((highcharts_
 
               case 3:
                 response = _context.sent;
-                _this.results = response.data; //   const number = this.results.data.length;
-                // const number = response.data.data.length;
-                //   const number = 3;
-                //   console.log(number);
-                // let seriesTemp = [];
-                // for(let i=0; i<number; i++)
-                // {
-                //     this.series.push({
-                //         name: this.results.data.data[i].name,
-                //         data: this.results.data.data[i].data,
-                //     }); // end push
-                // } // end for
+                _this.results = response.data;
+                number = response.data.data.length;
 
-                _context.next = 11;
+                for (i = 0; i < number; i++) {
+                  _this.series.push({
+                    name: response.data.data[i].name,
+                    data: response.data.data[i].data
+                  }); // end push
+
+                } // end for
+                // set chart options
+
+
+                _this.chartOptions = {
+                  chart: {
+                    type: 'column'
+                  },
+                  title: {
+                    text: _this.results.title
+                  },
+                  xAxis: {
+                    categories: _this.results.categories
+                  },
+                  yAxis: {
+                    min: 0,
+                    title: {
+                      text: _this.results.y_axis
+                    },
+                    stackLabels: {
+                      enabled: true,
+                      style: {
+                        fontWeight: 'bold',
+                        color: // theme
+                        (highcharts__WEBPACK_IMPORTED_MODULE_1___default().defaultOptions.title.style) && (highcharts__WEBPACK_IMPORTED_MODULE_1___default().defaultOptions.title.style.color) || 'gray'
+                      }
+                    }
+                  },
+                  legend: {
+                    align: 'right',
+                    x: -30,
+                    verticalAlign: 'top',
+                    y: 25,
+                    floating: true,
+                    backgroundColor: (highcharts__WEBPACK_IMPORTED_MODULE_1___default().defaultOptions.legend.backgroundColor) || 'white',
+                    borderColor: '#CCC',
+                    borderWidth: 1,
+                    shadow: false
+                  },
+                  tooltip: {
+                    headerFormat: '<b>{point.x}</b><br/>',
+                    pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+                  },
+                  plotOptions: {
+                    column: {
+                      stacking: 'normal',
+                      dataLabels: {
+                        enabled: true
+                      }
+                    }
+                  },
+                  series: _this.series
+                };
+                highcharts__WEBPACK_IMPORTED_MODULE_1___default().chart(_this.$el, _this.chartOptions); // end chart
+
+                _context.next = 15;
                 break;
 
-              case 7:
-                _context.prev = 7;
+              case 11:
+                _context.prev = 11;
                 _context.t0 = _context["catch"](0);
                 console.log('error!!!!');
                 console.error(_context.t0);
 
-              case 11:
+              case 15:
               case "end":
                 return _context.stop();
             }
           }
-        }, _callee, null, [[0, 7]]);
+        }, _callee, null, [[0, 11]]);
       }))();
     } // end fetchData
-    // mapDataForChart(self) 
-    // {
-    //     const number = self.results.data.length;
-    //     // const number = response.data.data.length;
-    //     // const number = 3;
-    //     console.log(number);
-    //     // let seriesTemp = [];
-    //     for(let i=0; i<number; i++)
-    //     {
-    //         self.series.push({
-    //             name: self.results.data.data[i].name,
-    //             data: self.results.data.data[i].data,
-    //         }); // end push
-    //     } // end for
-    // } // end mapDataForChart
 
   } // end methods
 
--- resources/js/components/StackedColumnChart2.vue
+++ resources/js/components/StackedColumnChart2.vue
@@ -21,277 +21,96 @@
       return {
           results: [],
           series: [],
+          chartOptions: {}
       }
     },
 
-    computed: {
-        mapDataForChart() 
-        {
-            const number = this.results.data.length;
-            // const number = response.data.data.length;
-            // const number = 3;
-            console.log(number);
-            // let seriesTemp = [];
-
-            for(let i=0; i<number; i++)
-            {
-                this.series.push({
-                    name: this.results.data.data[i].name,
-                    data: this.results.data.data[i].data,
-                }); // end push
-            } // end for
-        } // end mapDataForChart
-    }, // end computed
-
     created() {
-      console.log("stacked column chart 2 created");
+    //   console.log("stacked column chart 2 created");
     
-    //   axios.get(this.url)
-    //         .then(response => {
-    //             this.results = response.data;
-                
-    //             const number = this.results.data.length;
-
-    //             console.log(number);
-    //             // let seriesTemp = [];
-
-    //             for(let i=0; i<number; i++)
-    //             {
-    //                 // console.log(this.results.data.data[i].name);
-    //                 // console.log(this.results.data.data[i].data);
-
-    //                 this.series.push({
-    //                     name: response.data.data[i].name,
-    //                     data: response.data.data[i].data,
-    //                     // name: this.results.data.data[i].name,
-    //                     // data: this.results.data.data[i].data,
-    //                 }); // end push
-    //             } // end for
-    //         }); //end axios get then
-      
-            // const number = response.data.data.length;
-            // const number = 3;
-     
-    //   this.mapDataForChart();
-
-      let self = this;
-
-    //   self.fetchData();
       this.fetchData();
-    //   self.mapDataForChart(self);    
-    //   this.mapDataForChart(this);    
-    //   this.mapDataForChart();    
     },
-
-    mounted() {
-      console.log("inside mounted");
-    //   this.mapDataForChart(this);
-
-    //   console.log(this.url);
-    //   let self = this;
-
-    //   let self = this;
-
-    console.log('why is this undefined???');
-    console.log(this.results);
-    // console.log(self.results);
-    // console.log(self.results.title);
-
-    const chartOptions = {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: this.results.title
-        },
-        xAxis: {
-            categories: this.results.categories
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: this.results.y_axis
-            },
-            stackLabels: {
-                enabled: true,
-                style: {
-                    fontWeight: 'bold',
-                    color: ( // theme
-                        Highcharts.defaultOptions.title.style &&
-                        Highcharts.defaultOptions.title.style.color
-                    ) || 'gray'
-                }
-            }
-        },
-        legend: {
-            align: 'right',
-            x: -30,
-            verticalAlign: 'top',
-            y: 25,
-            floating: true,
-            backgroundColor:
-                Highcharts.defaultOptions.legend.backgroundColor || 'white',
-            borderColor: '#CCC',
-            borderWidth: 1,
-            shadow: false
-        },
-        tooltip: {
-            headerFormat: '<b>{point.x}</b><br/>',
-            pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-        },
-        plotOptions: {
-            column: {
-                stacking: 'normal',
-                dataLabels: {
-                    enabled: true
-                }
-            }
-        },
-        series: this.series,
-    };
-
-    // test chart
-    Highcharts.chart(this.$el, {
-
-        title: {
-            text: this.results.title
-        },
-
-        subtitle: {
-            text: 'Source: thesolarfoundation.com'
-        },
-
-        yAxis: {
-            title: {
-                text: 'Number of Employees'
-            }
-        },
-
-        xAxis: {
-            accessibility: {
-                rangeDescription: 'Range: 2010 to 2017'
-            }
-        },
-
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-        },
-
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2010
-            }
-        },
-
-        series: [{
-            name: 'Installation',
-            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-        }, {
-            name: 'Manufacturing',
-            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-        }, {
-            name: 'Sales & Distribution',
-            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-        }, {
-            name: 'Project Development',
-            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-        }, {
-            name: 'Other',
-            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-        }],
-
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-        }
-
-    });
-
-    // Highcharts.chart(this.$el, chartOptions); // end chart
-    }, // end mounted
 
     methods: {
         async fetchData() 
         {
-            // console.log("inside fetchData...");
-            // let self = this;
             try {
               const response = await axios.get(this.url);
               this.results = response.data;
   
-            //   const number = this.results.data.length;
+              const number = response.data.data.length;
 
-              // const number = response.data.data.length;
-            //   const number = 3;
-            //   console.log(number);
-              // let seriesTemp = [];
+                for(let i=0; i<number; i++)
+                {
+                    this.series.push({
+                        name: response.data.data[i].name,
+                        data: response.data.data[i].data,
+                    }); // end push
+                } // end for
 
-                // for(let i=0; i<number; i++)
-                // {
-                //     this.series.push({
-                //         name: this.results.data.data[i].name,
-                //         data: this.results.data.data[i].data,
-                //     }); // end push
-                // } // end for
-                
+                // set chart options
+                this.chartOptions = {
+                    chart: {
+                        type: 'column'
+                    },
+                    title: {
+                        text: this.results.title
+                    },
+                    xAxis: {
+                        categories: this.results.categories
+                    },
+                    yAxis: {
+                        min: 0,
+                        title: {
+                            text: this.results.y_axis
+                        },
+                        stackLabels: {
+                            enabled: true,
+                            style: {
+                                fontWeight: 'bold',
+                                color: ( // theme
+                                    Highcharts.defaultOptions.title.style &&
+                                    Highcharts.defaultOptions.title.style.color
+                                ) || 'gray'
+                            }
+                        }
+                    },
+                    legend: {
+                        align: 'right',
+                        x: -30,
+                        verticalAlign: 'top',
+                        y: 25,
+                        floating: true,
+                        backgroundColor:
+                            Highcharts.defaultOptions.legend.backgroundColor || 'white',
+                        borderColor: '#CCC',
+                        borderWidth: 1,
+                        shadow: false
+                    },
+                    tooltip: {
+                        headerFormat: '<b>{point.x}</b><br/>',
+                        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+                    },
+                    plotOptions: {
+                        column: {
+                            stacking: 'normal',
+                            dataLabels: {
+                                enabled: true
+                            }
+                        }
+                    },
+                    series: this.series,
+                };
+            
+                Highcharts.chart(this.$el, this.chartOptions); // end chart
             }
             catch(e) {
                 console.log('error!!!!')
                 console.error(e)
             }
         
-            // this.mapDataForChart(this);
-            
-            // axios.get(this.url).then(response => {
-                // console.log('inside axios then...');
-                
-                // this.results = response.data;
-                // self.results = response.data;
-                // console.log(response.data);
-                // console.log(data);
-                // console.log(this.results.title);
-
-                // this.series = seriesTemp;
-                // self.results = response.data;
-                // this.series = seriesTemp;
-            // }); // end axios
-            
-            // console.log('inside fecthData outside of axios...');
-            // console.log(this.results);
-        
         }, // end fetchData
     
-        // mapDataForChart(self) 
-        // {
-        //     const number = self.results.data.length;
-        //     // const number = response.data.data.length;
-        //     // const number = 3;
-        //     console.log(number);
-        //     // let seriesTemp = [];
-
-        //     for(let i=0; i<number; i++)
-        //     {
-        //         self.series.push({
-        //             name: self.results.data.data[i].name,
-        //             data: self.results.data.data[i].data,
-        //         }); // end push
-        //     } // end for
-        // } // end mapDataForChart
     }, // end methods
  
   } // end export default
